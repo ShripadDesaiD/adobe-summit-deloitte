78f5eae4a8f90b9aab355ac6c440cf8e2beb81f6
aamlathe <107908837+aamlathe@users.noreply.github.com>
2023-03-10 19:02:48 +0530

    Delete datalayer.js

--- datalayer.js
+++
@@ -1,9 +0,0 @@
-var locationURL = window.location.href;
-var pageName = "Abbvie";
-if(window.location.pathname != "/"){
- var interimVal = window.location.pathname.split("/")[2];
- pageName = interimVal.split(".html")[0];
-}
-console.log(pageName);
-console.log(locationURL);
-var digitalData = {"page":{"pageName":pageName, "URL":locationURL}};
